51681860cdf5b704beec42553d5313deefb1439b
Mailune <mailysrosereau@yahoo.fr>
2024-05-08 03:57:56 +0200

    Correction by using format and not f string

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i + 1, 10):
-        print(f"{i}{j}, " if i != 8 else f"{i}{j}\n", end="")
+        if i != 8:
+            print("{:d}{:d}, ".format(i, j), end='')
+        else:
+            print("{:d}{:d}\n".format(i, j), end='')
