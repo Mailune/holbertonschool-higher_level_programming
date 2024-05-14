6bf620011b1e1b9ad935ae97fd663f3495e06e75
Mailune <mailysrosereau@yahoo.fr>
2024-05-14 02:36:58 +0200

    Refactor to use explicit element indexing

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+        for element_index in range(len(row)):
+            print("{:d}".format(row[element_index]), end="")
+            if element_index != len(row) - 1:
+                print(" ", end="")
         print()
