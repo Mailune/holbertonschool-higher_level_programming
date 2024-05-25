62fd7af162f1f8b4e8fa296b5f578fb11dad306d
Mailune <mailysrosereau@yahoo.fr>
2024-05-25 09:08:27 +0200

    Refactor integer_validator method to use type() for type checking

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-""" Module for is_kind_of_class method"""
+"""
+7-base_geometry.py 
+
+Module for is_kind_of_class method"""
 
 
 class BaseGeometry:
     """
-    Base class representing geometry.
-
-    This class provides a base for geometric shapes.
+    The 'BaseGeometry' class.
     """
+
+    def __init__(self):
+        """Initialize the class (even if empty)."""
+        pass
 
     def area(self):
         """
-        Calculate the area of the geometric shape.
-
-        Raises:
-            Exception: Always raises an exception with the message
-                'area() is not implemented'.
+        Public instance method: def area(self):
+        raises an Exception with the message 'area() is not implemented'.
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Validate the value for being an integer and being greater than 0.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
+        Public instance method that validates value:
+        raise a TypeError if value is not an int
+        raise a ValueError if value <= 0
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
