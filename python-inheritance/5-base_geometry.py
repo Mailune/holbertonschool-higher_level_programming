a8267da9769757c41574e7e1d7c13ae246abffb3
Mailune <mailysrosereau@yahoo.fr>
2024-05-25 08:49:34 +0200

    empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Module for is_kind_of_class method"""
+
+
+class BaseGeometry:
+    """
+    Empty class representing base geometry.
+
+    This class serves as a base for geometric shapes.
+    It currently does not define any attributes or methods.
+    """
+    pass
