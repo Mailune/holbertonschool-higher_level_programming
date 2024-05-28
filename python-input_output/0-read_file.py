6f588ed7f40ae029e9d3457b1b7ec5fdc0f50be2
Mailune <mailysrosereau@yahoo.fr>
2024-05-28 04:03:20 +0200

    add the comments module and function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+This module contain a function read_file
+"""
+
 
 def read_file(filename=""):
+    """ a function that open a file"""
     with open(filename, 'r', encoding='utf-8') as fichier:
         contenu = fichier.read()
         print(contenu)
