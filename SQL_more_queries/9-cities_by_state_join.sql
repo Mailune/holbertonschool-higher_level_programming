b613fe498ef21286f4e763b56e67a738b791b77c
Mailune <mailysrosereau@yahoo.fr>
2024-06-20 22:42:37 +0200

    fix the script

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,4 @@
--- Select cities.id, cities.name, and corresponding state name
-SELECT
-    cities.id,
-    cities.name,
-    (SELECT name FROM states WHERE id = cities.state_id) AS state_name
-FROM
-    cities
-ORDER BY
-    cities.id ASC;
+-- List all cities contained in the database hbtn_0e_4_us by join between the cities and states tables
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
