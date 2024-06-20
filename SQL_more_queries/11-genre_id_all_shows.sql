d2faa6e2f247d7707cd67f4892c873c53d78bad7
Mailune <mailysrosereau@yahoo.fr>
2024-06-20 22:44:09 +0200

    fix the script

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,10 +1,6 @@
 -- Select shows and their linked genre IDs, display NULL if no genre exists
-SELECT
-    tv_shows.title,
-    tv_show_genres.genre_id
-FROM
-    tv_shows
-LEFT JOIN
-    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY
-    tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
