c391842da7726288c9b3ca558c79e83a9e16c759
Mailune <mailysrosereau@yahoo.fr>
2024-06-20 22:30:25 +0200

    Import the database dump of hbtn_0d_tvshows to your MySQL server: download (same as 10-genre_id_by_show.sql)

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- Select shows and their linked genre IDs, display NULL if no genre exists
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY
+    tv_shows.title ASC, tv_show_genres.genre_id ASC;
