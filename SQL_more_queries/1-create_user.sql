5d76bd72aef53ee1d2d6d7b08c5f88f287d5e141
Mailune <mailysrosereau@yahoo.fr>
2024-06-20 22:14:22 +0200

    script that creates the MySQL server user user_0d_1.

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,11 @@
+-- Create user user_0d_1 if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Set the password for user_0d_1 (in case it needs updating)
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes
+FLUSH PRIVILEGES;
