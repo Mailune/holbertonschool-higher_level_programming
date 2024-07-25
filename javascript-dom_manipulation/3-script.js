c258df898b508fd21c39fea0a60e73b400155f84
Mailune <mailysrosereau@yahoo.fr>
2024-07-26 01:16:21 +0200

    first commit

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleHeaderDiv = document.getElementById('toggle_header');
+    const header = document.querySelector('header');
+  
+    toggleHeaderDiv.addEventListener('click', () => {
+      if (header.classList.contains('red')) {
+        header.classList.remove('red');
+        header.classList.add('green');
+      } else {
+        header.classList.remove('green');
+        header.classList.add('red');
+      }
+    });
+  });
+  
